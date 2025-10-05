1dbce067eb09037dd811c87bc2f5acaa5aaf9879
Duy <lehaiduy2003@gmail.com>
2025-10-05 16:05:15 +0700

    fix: delete CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, HTTPException, Query
 from typing import List
 from .recommender import recommend
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Roommate AI Recommender")
 
@@ -11,18 +10,3 @@ def recommend_users(user_id: int = Query(...), top_n: int = Query(5)) -> List[di
     if not result:
         raise HTTPException(status_code=404, detail="Không tìm thấy user hoặc không có kết quả.")
     return result
-
-
-allowed_origins = [
-    "http://localhost:5173",
-    "https://cap2-fe.vercel.app"
-]
-
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=allowed_origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
